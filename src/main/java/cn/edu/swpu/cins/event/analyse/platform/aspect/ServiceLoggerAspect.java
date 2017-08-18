66e4a50682912f0300a69abc06f490b9830f3175
LLPP <410981254@qq.com>
2017-08-18 16:02:38 +0800

    use the actuator to monitor the application.

--- src/main/java/cn/edu/swpu/cins/event/analyse/platform/aspect/ServiceLoggerAspect.java
+++ src/main/java/cn/edu/swpu/cins/event/analyse/platform/aspect/ServiceLoggerAspect.java
@@ -1,7 +1,6 @@
 package cn.edu.swpu.cins.event.analyse.platform.aspect;
 
 import cn.edu.swpu.cins.event.analyse.platform.exception.OperationFailureException;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
@@ -20,54 +19,35 @@ public class ServiceLoggerAspect {
     public void logAop() {
     }
 
-    @Before("logAop()")
-    public void logBefore(JoinPoint joinPoint) {
-        String msg = "\njoinpoint: " + joinPoint.toString() +
-                "\nsignature name:" + joinPoint.getSignature().getName() +
-                "\ndeclaring type name:" + joinPoint.getSignature().getDeclaringTypeName() +
-                "\nargument count:" + joinPoint.getArgs().length +
-                "\ntype of arguments:";
-        for (Object args : joinPoint.getArgs()){
-            msg += ("\n" + args.getClass().getName() + "(value:" + args.toString() +")");
-        }
-        logger.info(msg);
-    }
-
-    @AfterReturning("logAop()")
-    public void logAfterReturning() {
-        System.out.println("返回通知AfterReturning");
-    }
-
-    @After("logAop() && args(name)")
-    public void logAfter(String name) {
-        System.out.println(name + "后置通知After");
-    }
-
-    @AfterThrowing("logAop()")
-    public void logAfterThrow() {
-        System.out.println("异常通知AfterThrowing");
-    }
-
     @Around("logAop()")
     public Object logAround(ProceedingJoinPoint jp) throws Throwable {
         try {
             String className = jp.getSignature().getDeclaringTypeName();
             String methodName = jp.getSignature().getName();
-            logger.info("\nCalling method: " + className + "." + methodName);
+            //print the information of method be called
+            String msg = "\nCalling method: " + className + "." + methodName;
+            msg += "\nArguments:";
 
+            //print the arguments;
+            for (Object args : jp.getArgs()){
+                msg += ("\n" + args.getClass().getName() + "(value:" + args.toString() +")");
+            }
+
+            logger.info(msg);
             //Record the cost time
             long startTime = System.currentTimeMillis();
             Object object = jp.proceed(jp.getArgs());
             long endTime = System.currentTimeMillis();
             long costTime = endTime - startTime;
-
             logger.info("\nFinished method: " + className + "." + methodName
                     + "\nCostTime: " + costTime + " millisecond");
+
             return object;
         } catch (Throwable throwable) {
             if (throwable instanceof OperationFailureException) {
-                logger.error(throwable.getMessage());
+                logger.error("Internal exception be threw:\n",throwable);
             }
+
             throw throwable;
         }
     }
